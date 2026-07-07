8a6ff9f3d17b117d18f603f01a96a6c2436d00ed
Muralim <muralim.czu@gmail.com>
2026-07-07 21:20:04 +0200

    finished the note service

--- Services/NoteService.cs
+++ Services/NoteService.cs
@@ -23,6 +23,26 @@ namespace practice_dotnet.Services
             var notes = await _context.Notes.Where(n => n.User.Id == userId).ToListAsync();
             return notes;
         }
-
+        public async Task<bool> AddNote(Note note)
+        {
+            _context.Notes.Add(note);
+            return await _context.SaveChangesAsync() > 0;
+        }
+        public async Task<bool> DeleteNote(int noteId)
+        {
+            var note = await _context.Notes.FindAsync(noteId);
+            if (note == null) return false;
+            _context.Notes.Remove(note);
+            return await _context.SaveChangesAsync() > 0;
+        }
+        public async Task<bool> UpdateNote(int noteId, Note updatedFields)
+        {
+            var existingNote = await _context.Notes.FindAsync(noteId);
+            if (existingNote == null) return false;
+            existingNote.Title = updatedFields.Title;
+            existingNote.Description = updatedFields.Description;
+            return await _context.SaveChangesAsync() > 0;
+        }
+        public async 
     }
 }
